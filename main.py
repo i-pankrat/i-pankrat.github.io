61edf3e014361836dc51dc0842cb1b56d1c83c9c
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-11-17 19:05:27 +0300

    Fix empty fields in testimonials

--- main.py
+++ main.py
@@ -19,23 +19,17 @@ def main_page():
     reviews = Review.query.order_by(Review.id.desc()).all()
 
     if request.method == "POST":
-        name = request.form.get('name', type=str, default='')
-        message = request.form.get('message', type=str, default='')
-        activity = request.form.get('activity', type=str, default='')
+        name = request.form.get('name', type=str, default='').strip()
+        message = request.form.get('message', type=str, default='').strip()
+        activity = request.form.get('activity', type=str, default='').strip()
 
-        # if not name:
-        #     return render_template('index.html', reviews=reviews)
-        # if not text:
-        #     return render_template('index.html', reviews=reviews)
-        # if not activity:
-        #     return render_template('index.html', reviews=reviews)
-        print(name + " " + activity + " " + message)
+        if len(name) < 3 or len(message) < 3 or len(activity) < 3:
+            return redirect(url_for('main_page'))
 
         try:
             p = Review(name=name, message=message, activity=activity)
             db.session.add(p)
             db.session.commit()
-            print("Added succesful!")
         except:
             print("Error while user add to database")
 
